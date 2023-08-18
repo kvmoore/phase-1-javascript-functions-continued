574bd7fad1e6c6150d7377a8d29c8927fd71f714
Kenneth <kvmoore097@gmail.com>
2023-08-17 20:03:27 -0500

    phase-1 functions lab cont

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// code your solution here
+
+function saturdayFun(activity = "roller-skate"){
+
+  return (`This Saturday, I want to ${activity}!`);
+}
+saturdayFun();
+
+
+
+const mondayWork = function (willDo = "go to the office") {
+
+    return (`This Monday, I will ${willDo}.`);
+} 
+mondayWork();
+
+
+
+function wrapAdjective(flair = "*"){
+  return function (compliment = "a hard worker"){
+    return `You are ${flair}${compliment}${flair}!`;
+
+
+  }
+}
+
+//wrapAdjective("%")("a dedicated programmer"); //=> "You are %a dedicated programmer%!"
+
+
